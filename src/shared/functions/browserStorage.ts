42030cd63fbd7b9910e9e01ce047270c1630d104
Rahul Kumar <cse.170310430@silicon.ac.in>
2022-09-03 14:40:53 +0530

    testing the github action

--- src/shared/functions/browserStorage.ts
+++ src/shared/functions/browserStorage.ts
@@ -2,20 +2,20 @@ import { RootState } from '@ReduxStore/store';
 
 const KEY = 'quiz-app';
 export function loadReduxState() {
-    try {
-        const serializedState = sessionStorage.getItem(KEY);
-        if (!serializedState) return undefined;
-        return JSON.parse(serializedState);
-    } catch (e) {
-        return undefined;
-    }
+	try {
+		const serializedState = sessionStorage.getItem(KEY);
+		if (!serializedState) return undefined;
+		return JSON.parse(serializedState);
+	} catch (e) {
+		return undefined;
+	}
 }
 
 export async function saveReduxState(state: RootState) {
-    try {
-        const serializedState = JSON.stringify(state);
-        sessionStorage.setItem(KEY, serializedState);
-    } catch (e) {
-        // Ignore
-    }
+	try {
+		const serializedState = JSON.stringify(state);
+		sessionStorage.setItem(KEY, serializedState);
+	} catch (e) {
+		// Ignore the error
+	}
 }
